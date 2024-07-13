2f119e6660a1fafc325fbd1bb5dc22576b4ed526
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-07-14 05:58:25 +0800

    added a counter for the button clicks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use gtk::{glib, glib::clone, Application, ApplicationWindow, Button, Label};
+use std::cell::Cell;
+// use std::rc::Rc;
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
 
@@ -15,25 +17,46 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    // create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
+    // create a button
+    let counter_lbl = Label::builder()
+        .label("You pressed the button 0 times!")
+        .build();
+
+    // create a button
+    let btn = Button::builder()
+        .label("Click me!")
+        .margin_top(12)
+        .build();
+
+    // reference-counted object with inner-mutability
+    // let number = Rc::new(Cell::new(0));
+    let number = Cell::new(0);
+
+    // connect callbacks
+    btn.connect_clicked(clone!(
+        #[weak]
+        counter_lbl,
+        move |_| {
+            number.set(number.get() + 1);
+            counter_lbl.set_label(&format!("You pressed the button {} times!", number.get()));
+        }
+    ));
+
+    let gtk_box = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
-
-    // connect to "clicked" signal of `button`
-    button.connect_clicked(|button| {
-        button.set_label("Hello world!");
-    });
+    gtk_box.append(&counter_lbl);
+    gtk_box.append(&btn);
 
     // create a window and set the title
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My Gtk App")
-        .child(&button)
+        .child(&gtk_box)
         .build();
 
     // present the window
